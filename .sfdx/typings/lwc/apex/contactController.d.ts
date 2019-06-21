c86edb83cba82c81a99118b98289c8d428921432
yash-pal <highpoint2218@gmail.com>
2019-06-21 11:01:03 +0530

    Functionality Updated

--- .sfdx/typings/lwc/apex/contactController.d.ts
+++ .sfdx/typings/lwc/apex/contactController.d.ts
@@ -2,8 +2,11 @@ declare module "@salesforce/apex/contactController.getContact" {
   export default function getContact(): Promise<any>;
 }
 declare module "@salesforce/apex/contactController.saveNote" {
-  export default function saveNote(param: {contactId: any}): Promise<any>;
+  export default function saveNote(param: {contactId: any, inputText: any}): Promise<any>;
 }
 declare module "@salesforce/apex/contactController.Accept" {
   export default function Accept(param: {contactId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/contactController.getCount" {
+  export default function getCount(param: {searchString: any}): Promise<any>;
 }
--- force-app/main/default/lwc/lwcTable/lwcTable.js
+++ force-app/main/default/lwc/lwcTable/lwcTable.js
@@ -1,4 +1,4 @@
-import { LightningElement,wire, api} from 'lwc';
+import { LightningElement, wire, api ,track} from 'lwc';
 import getContact from '@salesforce/apex/contactController.getContact';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import saveNote from '@salesforce/apex/contactController.saveNote';
@@ -6,73 +6,80 @@ import Accept from '@salesforce/apex/contactController.Accept';
 
 
 export default class LwcAssignment extends LightningElement {
-   @api error;
-   @api bShowModal = false;
-   @api openmodal;
-   @api recordId;
-   @api ParentId;
+    @api error;
+    @api bShowModal = false;
+    @api openmodal;
+    @api recordId;
+    @api ParentId;
+    @track searchKey; 
+    @api currentpage;  
+    isSearchChangeExecuted = false;
+    localCurrentPage = null;   
+    @api result;
 
 
     @wire(getContact)
     contacts;
 
 
-    openModal(event) {   
-    this.recordId= event.target.value;
-      /*eslint-disable no-console */
-      
-    // to open modal window set 'bShowModal' tarck value as true
-    this.bShowModal = true;
-    console.log("this.recordId "+ this.recordId); 
+    openModal(event) {
+        this.recordId = event.target.value;
+        /*eslint-disable no-console */
+
+        // to open modal window set 'bShowModal' tarck value as true
+        this.bShowModal = true;
+        console.log("this.recordId " + this.recordId);
     }
 
-    closeModal() { 
+    closeModal() {
         /*eslint-disable no-console */
-      console.log("hello");   
-    // to close modal window set 'bShowModal' track value as false
-    this.bShowModal = false;
-    
+        console.log("hello");
+        // to close modal window set 'bShowModal' track value as false
+        this.bShowModal = false;
+
     }
     saveMethod() {
-     /*eslint-disable no-console */
-     console.log("this.recordId "+ this.recordId); 
-    saveNote({contactId:this.recordId})
-    .then(() => {
-    const evt = new ShowToastEvent({
-        title: "Record Updated",
-        //message: "Response Saved ",
-        variant: 'Success',
-        
-        mode: 'dismissable'
-    });
-    this.dispatchEvent(evt);
-    this.closeModal();
+         const result =this.template.querySelector('.inputText');
+         this.result=result.value;
+        /*eslint-disable no-console */
+        console.log('value of input '+ this.result);
+        /*eslint-disable no-console */
+        console.log("this.recordId " + this.recordId);
+        saveNote({ contactId: this.recordId,inputText:this.result })
+            .then(() => {
+                const evt = new ShowToastEvent({
+                    title: "Record Updated",
+                    variant: 'Success',
 
-    }).catch(error => {
-        this.dispatchEvent(
-            new ShowToastEvent({
-                title: 'Error creating record',
-                message: error.body.message,
-                variant: 'error'
-            })
-        );
-    });
-        
+                    mode: 'dismissable'
+                });
+                this.dispatchEvent(evt);
+                this.closeModal();
+
+            }).catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error creating record',
+                        message: error.body.message,
+                        variant: 'error'
+                    })
+                );
+            });
+
     }
 
 
-    approvalMethod(event){
-        this.recordId= event.target.value;
-         /*eslint-disable no-console */
-         console.log("this.recordId "+ this.recordId);
+    approvalMethod(event) {
+        this.recordId = event.target.value;
+        /*eslint-disable no-console */
+        console.log("this.recordId " + this.recordId + "Hello World");
 
-        Accept({contactId:this.recordId})
+        Accept({ contactId: this.recordId })
 
             .then(() => {
                 const evnt = new ShowToastEvent({
-                    title:"Request Approved",
-                    //message:"Apporved Contact",
-                    variant:'Success'
+                    title: "Request Approved",
+                    variant: 'Success'
                 });
                 this.dispatchEvent(evnt);
             }).catch(error => {
@@ -84,6 +91,25 @@ export default class LwcAssignment extends LightningElement {
                     })
                 );
             });
+
+    }
+
+
+    handleKeyChange(event) {  
+        if (this.searchKey !== event.target.value) {  
+          this.isSearchChangeExecuted = false;  
+          this.searchKey = event.target.value;  
+          this.currentpage = 1;  
+        }  
+    }
     
-    }  
+    
+    renderedCallback() {  
+        // This line added to avoid duplicate/multiple executions of this code.  
+        if (this.isSearchChangeExecuted && (this.localCurrentPage === this.currentpage)) {  
+          return;  
+        }  
+        this.isSearchChangeExecuted = true;  
+        this.localCurrentPage = this.currentpage;
+    }
 }
